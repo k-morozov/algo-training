971894fbe6ab4c752d5b1837b8f9db027e6369a7
Konstantin Morozov <konstantin.morozov@openmedia.co>
2021-03-25 21:31:26 +0300

    first commit

---
+++ src/main/java/ru/acm/SummaDigit.java
@@ -0,0 +1,143 @@
+package ru.acm;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.io.StreamTokenizer;
+
+public class SummaDigit {
+    public static void main(String[] args) {
+        Finder finder = new Finder();
+
+        Stream stream = new Stream();
+
+        int countOperations = stream.nextInt();
+        int summaSimple, summaSquare;
+
+        for (int i = 0; i < countOperations; i++) {
+            summaSimple = stream.nextInt();
+            summaSquare = stream.nextInt();
+            String result = finder.findNumber(summaSimple, summaSquare);
+                stream.getOut().println(result);
+                stream.getOut().flush();
+        }
+    }
+}
+
+class Data {
+    public Data() {
+        value = 0;
+        length = 0;
+    }
+    public int value;
+    public int length;
+}
+
+class Finder {
+    private final int maxSummaSimple = 900;
+    private final int maxSummaSquare = 8100;
+
+    private final Data[][] dp;
+
+    public Finder() {
+        dp = new Data[maxSummaSimple+9][maxSummaSquare+81];
+
+        for (int i = 1; i < 10; i++) {
+            dp[i][i*i] = new Data();
+            dp[i][i*i].value = i;
+            dp[i][i*i].length = 1;
+        }
+
+        for (int i = 1; i < maxSummaSimple; i++) {
+            for (int j = 1; j < maxSummaSquare; j++) {
+                if (dp[i][j] != null && dp[i][j].value != 0) {
+                    for (int k = 1; k < 10; k++) {
+                        if (dp[i + k][j + k * k] == null) {
+                            dp[i + k][j + k * k] = new Data();
+
+                            dp[i + k][j + k * k].value = k;
+                            dp[i + k][j + k * k].length = dp[i][j].length + 1;
+                        } else if (dp[i + k][j + k * k].length >= dp[i][j].length + 1) {
+                            if (dp[i + k][j + k * k].value >= k) {
+                                dp[i + k][j + k * k].value = k;
+                            }
+                            dp[i + k][j + k * k].length = dp[i][j].length + 1;
+                        }
+                    }
+
+                }
+            }
+        }
+    }
+
+    public String findNumber(int summaDigit, int summaSquare) {
+        if (isValid(summaDigit, summaSquare)) {
+            return getNumber(summaDigit, summaSquare);
+        }
+
+        return "No solution";
+    }
+
+    private boolean isValid(int summaDigit, int summaSquare) {
+        if (summaDigit < 0 || summaDigit > maxSummaSimple) {
+            return false;
+        }
+
+        if (summaSquare < 0 || summaSquare > maxSummaSquare) {
+            return false;
+        }
+
+        if (dp[summaDigit][summaSquare] == null) {
+            return false;
+        }
+
+        if (dp[summaDigit][summaSquare].value == 0) {
+            return false;
+        }
+
+        if (dp[summaDigit][summaSquare].length > 100) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public String getNumber(int summaDigit, int summaSquare) {
+        StringBuilder number = new StringBuilder();
+
+        while (summaDigit > 0 && summaSquare > 0) {
+            int value = dp[summaDigit][summaSquare].value;
+            if (value == 0) {
+                break;
+            }
+            summaDigit -= value;
+            summaSquare -= value * value;
+            number.append(value);
+        }
+
+        return number.toString();
+    }
+}
+
+class Stream {
+    private final StreamTokenizer in;
+    private final PrintWriter out;
+
+    public Stream() {
+        in = new StreamTokenizer(new BufferedReader(new InputStreamReader(System.in)));
+        out = new PrintWriter(System.out);
+    }
+
+    public int nextInt() {
+        try {
+            in.nextToken();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return (int)in.nval;
+    }
+
+    public PrintWriter getOut() {
+        return out;
+    }
+}
---
+++ src/test/java/ru/acm/FinderTest.java
@@ -0,0 +1,162 @@
+package ru.acm;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FinderTest {
+    private final Finder finder = new Finder();
+    private final String noSolution = "No solution";
+
+    @Test
+    void Sum_9_SumSq_81() {
+        int summaDigit = 9;
+        int summaSquare = 81;
+        String result = finder.findNumber(summaDigit, summaSquare);
+        String expected = "9";
+
+        assertEquals(result, expected);
+    }
+
+    @Test
+    void Sum_12_SumSq_9() {
+        int summaDigit = 12;
+        int summaSquare = 9;
+        String result = finder.findNumber(summaDigit, summaSquare);
+        String expected = noSolution;
+
+        assertEquals(result, expected);
+    }
+
+    @Test
+    void Sum_6_SumSq_10() {
+        int summaDigit = 6;
+        int summaSquare = 10;
+        String result = finder.findNumber(summaDigit, summaSquare);
+        String expected = "1122";
+
+        assertEquals(result, expected);
+    }
+
+    @Test
+    void Sum_7_SumSq_9() {
+        int summaDigit = 7;
+        int summaSquare = 9;
+        String result = finder.findNumber(summaDigit, summaSquare);
+        String expected = "111112";
+
+        assertEquals(result, expected);
+    }
+
+    @Test
+    void Sum_45_SumSq_285() {
+        int summaDigit = 45;
+        int summaSquare = 285;
+        String result = finder.findNumber(summaDigit, summaSquare);
+        String expected = "14667777";
+
+        assertEquals(result, expected, "check that answer is min length (between some answers))");
+    }
+
+    @Test
+    void Sum_456_SumSq_2120() {
+        int summaDigit = 456;
+        int summaSquare = 2120;
+        String result = finder.findNumber(summaDigit, summaSquare);
+        String expected = "1334444444444444444444444444444444444445555555555555555555555555555555555555555555555555555555555555";
+
+        assertEquals(result, expected);
+    }
+
+    @Test
+    void Sum_456_SumSq_4081() {
+        int summaDigit = 456;
+        int summaSquare = 4081;
+        String result = finder.findNumber(summaDigit, summaSquare);
+        String expected = noSolution;
+
+        assertEquals(result, expected);
+    }
+
+    @Test
+    void Sum_3_SumSq_3() {
+        int summaDigit = 3;
+        int summaSquare = 3;
+        String result = finder.findNumber(summaDigit, summaSquare);
+        String expected = "111";
+
+        assertEquals(result, expected);
+    }
+
+    @Test
+    void Sum_900_SumSq_8100() {
+        int summaDigit = 900;
+        int summaSquare = 8100;
+        String result = finder.findNumber(summaDigit, summaSquare);
+        String expected = "9999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999";
+
+        assertEquals(result, expected);
+    }
+
+    @Test
+    void Sum_456_SumSq_2119() {
+        int summaDigit = 456;
+        int summaSquare = 2119;
+        String result = finder.findNumber(summaDigit, summaSquare);
+        String expected = noSolution;
+
+        assertEquals(result, expected);
+    }
+
+    @Test
+    void Sum_456_SumSq_3456() {
+        int summaDigit = 456;
+        int summaSquare = 3456;
+        String result = finder.findNumber(summaDigit, summaSquare);
+        String expected = "2677777777777777777777777788888888888888888888888888888888888";
+
+        assertEquals(result, expected);
+    }
+
+    @Test
+    void Sum_901_SumSq_3456() {
+        int summaDigit = 901;
+        int summaSquare = 3456;
+        String result = finder.findNumber(summaDigit, summaSquare);
+        String expected = noSolution;
+
+        assertEquals(result, expected);
+    }
+
+    @Test
+    void Sum_M1_SumSq_3456() {
+        int summaDigit = -1;
+        int summaSquare = 3456;
+        String result = finder.findNumber(summaDigit, summaSquare);
+        String expected = noSolution;
+
+        assertEquals(result, expected);
+    }
+
+    @Test
+    void Sum_456_SumSq_8101() {
+        int summaDigit = 456;
+        int summaSquare = 8101;
+        String result = finder.findNumber(summaDigit, summaSquare);
+        String expected = noSolution;
+
+        assertEquals(result, expected);
+    }
+    @Test
+    void Sum_456_SumSq_M1() {
+        int summaDigit = 456;
+        int summaSquare = -1;
+        String result = finder.findNumber(summaDigit, summaSquare);
+        String expected = noSolution;
+
+        assertEquals(result, expected);
+    }
+
+}
