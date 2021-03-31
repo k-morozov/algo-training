0c6da950e863ad652a0ef08f0a5e5801335f8b64
Konstantin Morozov <konstantin.morozov@openmedia.co>
2021-03-31 08:53:47 +0300

    1298. Конь

--- src/main/java/ru/acm/Chess/chess.java
+++ src/main/java/ru/acm/Chess/chess.java
@@ -14,11 +14,11 @@ public class chess {
         int size = stream.nextInt();
 
         Foo foo = new Foo(size);
+        foo.init();
 
         boolean result = foo.check();
 
         if(result) {
-//            System.out.println("POSSIBLE");
             foo.print(stream);
         } else {
             System.out.println("IMPOSSIBLE");
@@ -27,12 +27,12 @@ public class chess {
 }
 
 class Pair {
-    public final int let;
-    public final int dig;
+    public final int left;
+    public final int right;
 
     Pair(int left, int right) {
-        this.let = left;
-        this.dig = right;
+        this.left = left;
+        this.right = right;
     }
 }
 
@@ -42,25 +42,62 @@ class Foo {
     private char[] letters = {'a','b','c','d','e','f','g','h','i','j','k'};
     private int countFlag;
     private ArrayList<Pair> resultPos;
+    private int[][] chessboard;
+    private Pair[] direction = {
+            new Pair (2,1),
+            new Pair (-2,1),
+            new Pair (2,-1),
+            new Pair (-2,-1),
+            new Pair (1,2),
+            new Pair (-1,2),
+            new Pair (1,-2),
+            new Pair (-1,-2),
+    };
 
     Foo(int size) {
         this.size = size;
         countFlag = size * size;
         data = new int[this.size][this.size];
         resultPos = new ArrayList<>();
+        chessboard= new int[size][size];
     }
 
     public void print(Stream stream) {
         for (Pair pos: resultPos) {
-//            System.out.println(letters[pos.let] + "" + (pos.dig+1));
-            stream.getOut().println(letters[pos.let] + "" + (pos.dig+1));
+//            System.out.println(leftters[pos.left] + "" + (pos.right+1));
+            stream.getOut().println(letters[pos.left] + "" + (pos.right+1));
             stream.getOut().flush();
         }
     }
 
     public boolean check() {
-        boolean result = getShift(0, 0);
-        return result;
+        return getShift(0, 0);
+    }
+
+    public void init() {
+        for(int x = 0; x < size; x++) {
+            for(int y = 0; y < size; y++) {
+                int count=0;
+                for(int k = 0; k < direction.length ; k++) {
+                    int x1 = x + direction[k].left;
+                    int y1 = y + direction[k].right;
+
+                    if(x1>=0 && x1<size && y1>=0 && y1<size) {
+                        count++;
+                    }
+                }
+                chessboard[x][y] = count;
+            }
+        }
+
+//        System.out.println("===");
+//        for (int i = 0; i < size; i++) {
+//            for (int j = 0; j < size; j++) {
+//                System.out.print(chessboard[i][j] + " ");
+//            }
+//            System.out.println("");
+//        }
+//        System.out.println("===");
     }
 
     private boolean getShift(int x, int y) {
@@ -82,45 +119,7 @@ class Foo {
             return true;
         }
 
-        boolean result;
-
-        // up
-        result = getShift(x+1, y+2);
-        if (result) {
-            return true;
-        }
-
-        result = getShift(x-1, y+2);
-        if (result) {
-            return true;
-        }
-
-        // right
-        result = getShift(x+2, y+1);
-        if (result) {
-            return true;
-        }
-        result = getShift(x+2, y-1);
-        if (result) {
-            return true;
-        }
-
-        // down
-        result = getShift(x+1, y-2);
-        if (result) {
-            return true;
-        }
-        result = getShift(x-1, y-2);
-        if (result) {
-            return true;
-        }
-
-        // left
-        result = getShift(x-2, y+1);
-        if (result) {
-            return true;
-        }
-        result = getShift(x-2, y-1);
+        boolean result = checkAllSide(x,y);
         if (result) {
             return true;
         }
@@ -132,6 +131,86 @@ class Foo {
 
         return false;
 
+    }
+
+    private boolean checkAllSide(int x, int y) {
+        boolean result;
+
+        if (size != 6) {
+            int bestId = 100;
+            ArrayList<Pair> cases = new ArrayList<Pair>();
+
+            for (int k = 0; k < direction.length; k++) {
+                int x1 = x + direction[k].left;
+                int y1 = y + direction[k].right;
+
+                if (x1 >= 0 && x1 < size && y1 >= 0 && y1 < size) {
+                    if (data[x1][y1] == 0) {
+                        if (bestId > chessboard[x1][y1]) {
+                            bestId = chessboard[x1][y1];
+                            cases.clear();
+                            cases.add(new Pair(x1, y1));
+                        } else {
+                            if (bestId == chessboard[x1][y1]) {
+                                cases.add(new Pair(x1, y1));
+                            }
+                        }
+                    }
+                }
+            }
+
+            for (int i = 0; i < cases.size(); i++) {
+                result = getShift(cases.get(i).left, cases.get(i).right);
+                if (result) {
+                    return true;
+                }
+            }
+        } else {
+
+
+            // ===============================================
+            // up
+            result = getShift(x + 1, y + 2);
+            if (result) {
+                return true;
+            }
+
+            result = getShift(x - 1, y + 2);
+            if (result) {
+                return true;
+            }
+
+            // right
+            result = getShift(x + 2, y + 1);
+            if (result) {
+                return true;
+            }
+            result = getShift(x + 2, y - 1);
+            if (result) {
+                return true;
+            }
+
+            // down
+            result = getShift(x + 1, y - 2);
+            if (result) {
+                return true;
+            }
+            result = getShift(x - 1, y - 2);
+            if (result) {
+                return true;
+            }
+
+            // left
+            result = getShift(x - 2, y + 1);
+            if (result) {
+                return true;
+            }
+            result = getShift(x - 2, y - 1);
+            if (result) {
+                return true;
+            }
+        }
+        return false;
     }
 }
 
